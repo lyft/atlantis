b6d89e073135c93cd1d1b76722f0114407c34241
Sarvar Muminov <sarvarmuminov@lyft.com>
2020-10-26 15:59:53 -0700

    Renaming PolicyRunnerStep to PolicyCheckStepRunner

--- server/server.go
+++ server/server.go
@@ -404,7 +404,7 @@ func NewServer(userConfig UserConfig, config Config) (*Server, error) {
 				CommitStatusUpdater: commitStatusUpdater,
 				AsyncTFExec:         terraformClient,
 			},
-			PolicyStepRunner: &runtime.PolicyRunnerStep{},
+			PolicyCheckStepRunner: &runtime.PolicyCheckStepRunner{},
 			ApplyStepRunner: &runtime.ApplyStepRunner{
 				TerraformExecutor:   terraformClient,
 				CommitStatusUpdater: commitStatusUpdater,
