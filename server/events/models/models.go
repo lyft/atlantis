3adca150536046344d2a10819fd2bea810781a23
Isata Sankoh <isatas@lyft.com>
2021-07-16 18:45:28 -0400

    fixed test

--- server/events/models/models.go
+++ server/events/models/models.go
@@ -436,7 +436,7 @@ func (p ProjectCommandContext) GetShowResultFileName() string {
 }
 
 // Gets a unique identifier for the current pull request as a single string
-func (c *ProjectCommandContext) PullInfo() string {
+func (p *ProjectCommandContext) PullInfo() string {
 	return fmt.Sprintf("%s/%d/%s", c.BaseRepo.FullName, c.Pull.Num, c.ProjectName)
 }
 
