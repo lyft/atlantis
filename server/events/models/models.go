d6611a1a87656bc2069f7233094233c562e665fe
Isata Sankoh <isatas@lyft.com>
2021-07-19 11:51:28 -0400

    Fixing failed tests

--- server/events/models/models.go
+++ server/events/models/models.go
@@ -437,7 +437,7 @@ func (p ProjectCommandContext) GetShowResultFileName() string {
 
 // Gets a unique identifier for the current pull request as a single string
 func (p *ProjectCommandContext) PullInfo() string {
-	return fmt.Sprintf("%s/%d/%s", c.BaseRepo.FullName, c.Pull.Num, c.ProjectName)
+	return fmt.Sprintf("%s/%d/%s", p.BaseRepo.FullName, p.Pull.Num, p.ProjectName)
 }
 
 // SplitRepoFullName splits a repo full name up into its owner and repo
