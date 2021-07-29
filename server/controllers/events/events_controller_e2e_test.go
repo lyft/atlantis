b0fc54de3d13556daf711a7e994124eba8990999
Isata Sankoh <isatas@lyft.com>
2021-07-29 16:09:04 -0400

    Testing

--- server/controllers/events/events_controller_e2e_test.go
+++ server/controllers/events/events_controller_e2e_test.go
@@ -666,7 +666,7 @@ func setupE2E(t *testing.T, repoDir string) (events_controllers.VCSEventsControl
 	e2eStatusUpdater := &events.DefaultCommitStatusUpdater{Client: e2eVCSClient}
 	e2eGithubGetter := mocks.NewMockGithubPullGetter()
 	e2eGitlabGetter := mocks.NewMockGitlabMergeRequestGetter()
-	tempchan := make(chan<- *models.TerraformOutputLine)
+	tempchan := make(chan *models.TerraformOutputLine)
 
 	// Real dependencies.
 	logger := logging.NewNoopLogger(t)
@@ -793,6 +793,7 @@ func setupE2E(t *testing.T, repoDir string) (events_controllers.VCSEventsControl
 		WorkingDir:          workingDir,
 		Webhooks:            &mockWebhookSender{},
 		WorkingDirLocker:    locker,
+		TerraformOutputChan: tempchan,
 	}
 
 	dbUpdater := &events.DBUpdater{
@@ -893,6 +894,11 @@ func setupE2E(t *testing.T, repoDir string) (events_controllers.VCSEventsControl
 
 	repoAllowlistChecker, err := events.NewRepoAllowlistChecker("*")
 	Ok(t, err)
+
+	go func() {
+		for _ = range tempchan {
+		}
+	}()
 
 	ctrl := events_controllers.VCSEventsController{
 		TestingMode:   true,
