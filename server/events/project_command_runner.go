804c6ba330cafe6a3760822efede041f4f9cb60f
Isata Sankoh <isatas@lyft.com>
2021-08-17 13:42:32 -0400

    Removing clearing for apply workflow

--- server/events/project_command_runner.go
+++ server/events/project_command_runner.go
@@ -340,7 +340,6 @@ func (p *DefaultProjectCommandRunner) doApply(ctx models.ProjectCommandContext) 
 	}
 	defer unlockFn()
 
-	p.ProjectCmdOutputHandler.Clear(ctx)
 	outputs, err := p.runSteps(ctx.Steps, ctx, absPath)
 	p.Webhooks.Send(ctx.Log, webhooks.ApplyResult{ // nolint: errcheck
 		Workspace: ctx.Workspace,
