f19e019fc5560faca0f5ab64841422eccc82ae43
Aayush Gupta <aayushgupta@lyft.com>
2021-09-22 13:44:15 -0700

    Pulling [#1827-Updating client interface and adding ApprovalStatus model] from master

--- server/events/apply_requirement_handler.go
+++ server/events/apply_requirement_handler.go
@@ -4,7 +4,6 @@ import (
 	"github.com/runatlantis/atlantis/server/events/models"
 	"github.com/runatlantis/atlantis/server/events/yaml/raw"
 	"github.com/runatlantis/atlantis/server/events/yaml/valid"
-	"github.com/runatlantis/atlantis/server/feature"
 )
 
 //go:generate pegomock generate -m --package mocks -o mocks/mock_apply_handler.go ApplyRequirement
@@ -13,15 +12,14 @@ type ApplyRequirement interface {
 }
 
 type AggregateApplyRequirements struct {
-	WorkingDir       WorkingDir
-	FeatureAllocator feature.Allocator
+	WorkingDir WorkingDir
 }
 
 func (a *AggregateApplyRequirements) ValidateProject(repoDir string, ctx models.ProjectCommandContext) (failure string, err error) {
 	for _, req := range ctx.ApplyRequirements {
 		switch req {
 		case raw.ApprovedApplyRequirement:
-			if !ctx.PullReqStatus.Approved {
+			if !ctx.PullReqStatus.Approved.IsApproved {
 				return "Pull request must be approved by at least one person other than the author before running apply.", nil
 			}
 		// this should come before mergeability check since mergeability is a superset of this check.
@@ -38,12 +36,7 @@ func (a *AggregateApplyRequirements) ValidateProject(repoDir string, ctx models.
 				return "Default branch must be rebased onto pull request before running apply.", nil
 			}
 		case raw.UnlockedApplyRequirement:
-			shouldAllocate, err := a.FeatureAllocator.ShouldAllocate(feature.AtlantisLock, ctx.BaseRepo.FullName)
-			if err != nil {
-				ctx.Log.Err("unable to allocate for feature: %s, error: %s", feature.AtlantisLock, err)
-			}
-
-			if shouldAllocate && ctx.PullReqStatus.SQLocked {
+			if ctx.PullReqStatus.SQLocked {
 				return "Pull request must be unlocked using the 🔓  emoji before running apply.", nil
 			}
 		}
