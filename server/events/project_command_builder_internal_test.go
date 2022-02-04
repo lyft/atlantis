0145cf33d866a5e6bbfff3e3de3ec3555bdcd146
Sarvar Muminov <sarvarmuminov@lyft.com>
2022-02-04 13:39:13 -0800

    use correct method

--- server/events/project_command_builder_internal_test.go
+++ server/events/project_command_builder_internal_test.go
@@ -791,7 +791,7 @@ projects:
 			globalCfgPath := filepath.Join(tmp, "global.yaml")
 			Ok(t, ioutil.WriteFile(globalCfgPath, []byte(c.globalCfg), 0600))
 			parser := &config.ParserValidator{}
-			globalCfg, err := parser.ParseGlobalCfg(globalCfgPath, valid.NewGlobalCfg(false, false, false))
+			globalCfg, err := parser.ParseGlobalCfg(globalCfgPath, valid.NewGlobalCfgFromArgs(valid.GlobalCfgArgs{}))
 			Ok(t, err)
 
 			if c.repoCfg != "" {
