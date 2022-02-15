f8f204d5445a41f0cf70bfba9419c4d0c922f7c1
Aayush Gupta <aayushgupta@lyft.com>
2022-02-15 10:17:52 -0800

    Fix typo

--- server/controllers/websocket/instrumented_mux.go
+++ server/controllers/websocket/instrumented_mux.go
@@ -12,25 +12,25 @@ type InstrumentedMultiplexor struct {
 	Multiplexor
 
 	numWsConnections int64
-	NumWsConnection  tally.Gauge
+	NumWsConnections tally.Gauge
 	logger           logging.SimpleLogging
 }
 
 func NewInstrumentedMultiplexor(multiplexor Multiplexor, statsScope tally.Scope, logger logging.SimpleLogging) Multiplexor {
 	return &InstrumentedMultiplexor{
-		Multiplexor:     multiplexor,
-		NumWsConnection: statsScope.SubScope("getprojectjobs").SubScope("websocket").Gauge("connections"),
-		logger:          logger,
+		Multiplexor:      multiplexor,
+		NumWsConnections: statsScope.SubScope("getprojectjobs").SubScope("websocket").Gauge("connections"),
+		logger:           logger,
 	}
 }
 
 func (i *InstrumentedMultiplexor) Handle(w http.ResponseWriter, r *http.Request) error {
 	atomic.AddInt64(&i.numWsConnections, 1)
-	i.NumWsConnection.Update(float64(i.numWsConnections))
+	i.NumWsConnections.Update(float64(i.numWsConnections))
 
 	defer func() {
 		atomic.AddInt64(&i.numWsConnections, -1)
-		i.NumWsConnection.Update(float64(i.numWsConnections))
+		i.NumWsConnections.Update(float64(i.numWsConnections))
 	}()
 
 	return i.Multiplexor.Handle(w, r)
