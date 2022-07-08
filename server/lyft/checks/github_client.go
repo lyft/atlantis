3ff0be54bf0aba1bdd44d22a7dbf36f093cc15bf
Aayush Gupta <aayushgupta@lyft.com>
2022-07-08 09:52:13 -0700

    Add support to handle in-flight PRs

--- server/lyft/checks/github_client.go
+++ server/lyft/checks/github_client.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/pkg/errors"
+	"github.com/runatlantis/atlantis/server/events/models"
 	"github.com/runatlantis/atlantis/server/events/vcs"
 	"github.com/runatlantis/atlantis/server/events/vcs/types"
 	"github.com/runatlantis/atlantis/server/logging"
@@ -29,5 +31,32 @@ func (c *ChecksClientWrapper) UpdateStatus(ctx context.Context, request types.Up
 		return c.GithubClient.UpdateStatus(ctx, request)
 	}
 
+	// Get all commit statuses and check if the commit status for this operation is pending
+	// and mirror the checks status. This is possible when PRs are in-flight during rollout.
+	// [WENGINES-4643] - Clean up after github checks is stable.
+	statuses, err := c.GithubClient.GetRepoStatuses(request.Repo, models.PullRequest{
+		HeadCommit: request.Ref,
+	})
+	if err != nil {
+		return errors.Wrap(err, "retrieving repo statuses")
+	}
+
+	for _, status := range statuses {
+		// Skip if name does not match or the state is same
+		if *status.Context != request.StatusName || isSameState(*status.State, request.State) {
+			continue
+		}
+		c.GithubClient.UpdateStatus(ctx, request)
+	}
+
 	return c.GithubClient.UpdateChecksStatus(ctx, request)
+}
+
+func isSameState(statusState string, requestState models.CommitStatus) bool {
+	if requestState == models.PendingCommitStatus && statusState == "pending" ||
+		requestState == models.FailedCommitStatus && statusState == "failure" ||
+		requestState == models.SuccessCommitStatus && statusState == "success" {
+		return true
+	}
+	return false
 }
