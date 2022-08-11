5dc4f0da0f6ca6031bdcd31fd7bbdf83bd9f62cf
Samra Belachew <sbelachew@lyft.com>
2022-08-11 09:12:57 -0700

    comment

--- server/neptune/workflows/internal/deploy/terraform.go
+++ server/neptune/workflows/internal/deploy/terraform.go
@@ -10,6 +10,8 @@ type TerraformRequest = terraform.Request
 
 var TerraformTaskQueue = terraform.TaskQueue
 
+// To be able to execute a child workflow within the deploy workflow and avoid import cycles, terraform workflow
+// is defined within deploy package
 func Terraform(ctx workflow.Context, request TerraformRequest) error {
 	return terraform.Workflow(ctx, request)
 }
