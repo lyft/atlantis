4a53866f034a4cc8657a768ac0667426b95b1587
Nish Krishnan <nishk@lyft.com>
2022-08-17 07:47:31 -0700

    app config plus registration.

--- server/neptune/workflows/deploy.go
+++ server/neptune/workflows/deploy.go
@@ -1,8 +1,12 @@
 package workflows
 
 import (
+	"github.com/palantir/go-githubapp/githubapp"
+	"github.com/pkg/errors"
+	"github.com/runatlantis/atlantis/server/neptune/workflows/internal/activities"
 	"github.com/runatlantis/atlantis/server/neptune/workflows/internal/deploy"
 	"github.com/runatlantis/atlantis/server/neptune/workflows/internal/deploy/revision"
+	"github.com/uber-go/tally/v4"
 	"go.temporal.io/sdk/workflow"
 )
 
@@ -16,6 +20,22 @@ type DeployNewRevisionSignalRequest = revision.NewRevisionRequest
 var DeployTaskQueue = deploy.TaskQueue
 
 var DeployNewRevisionSignalID = deploy.NewRevisionSignalID
+
+type Activities struct {
+	activities.Deploy
+}
+
+func NewActivities(appConfig githubapp.Config, scope tally.Scope) (*Activities, error) {
+	deployActivities, err := activities.NewDeploy(appConfig, scope)
+
+	if err != nil {
+		return nil, errors.Wrap(err, "initializing deploy activities")
+	}
+
+	return &Activities{
+		Deploy: *deployActivities,
+	}, nil
+}
 
 func Deploy(ctx workflow.Context, request DeployRequest) error {
 	return deploy.Workflow(ctx, request)
