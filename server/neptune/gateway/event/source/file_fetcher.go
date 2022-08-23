89723e25584033e27f11bb1ee5db9f38890a238a
Samra Belachew <sbelachew@lyft.com>
2022-08-23 14:50:47 -0700

    finder

---
+++ server/neptune/gateway/event/source/file_fetcher.go
@@ -0,0 +1,66 @@
+package source
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/google/go-github/v45/github"
+	"github.com/palantir/go-githubapp/githubapp"
+	"github.com/runatlantis/atlantis/server/events/models"
+	"net/http"
+)
+
+type FileFetcher interface {
+	GetModifiedFilesFromCommit(ctx context.Context, repo models.Repo, sha string, installationToken int64) ([]string, error)
+}
+
+type GithubFileFetcher struct {
+	ClientCreator githubapp.ClientCreator
+}
+
+func (g *GithubFileFetcher) GetModifiedFilesFromCommit(ctx context.Context, repo models.Repo, sha string, installationToken int64) ([]string, error) {
+	var files []string
+	nextPage := 0
+	for {
+		opts := github.ListOptions{
+			PerPage: 300,
+		}
+		if nextPage != 0 {
+			opts.Page = nextPage
+		}
+		client, err := g.ClientCreator.NewInstallationClient(installationToken)
+		repositoryCommit, resp, err := client.Repositories.GetCommit(ctx, repo.Owner, repo.Name, sha, &opts)
+		if err != nil {
+			return files, err
+		}
+		if resp.StatusCode != http.StatusOK {
+			return nil, fmt.Errorf("error fetching repository contents: %s", resp.Status)
+		}
+		for _, f := range repositoryCommit.Files {
+			files = append(files, f.GetFilename())
+
+			// If the file was renamed, we'll want to run plan in the directory
+			// it was moved from as well.
+			if f.GetStatus() == "renamed" {
+				files = append(files, f.GetPreviousFilename())
+			}
+		}
+		if resp.NextPage == 0 {
+			break
+		}
+		nextPage = resp.NextPage
+	}
+	return files, nil
+}
+
+type MockSuccessFileFetcher struct{}
+
+func (m *MockSuccessFileFetcher) GetModifiedFilesFromCommit(ctx context.Context, _ models.Repo, _ string, _ int64) ([]string, error) {
+	return nil, nil
+}
+
+type MockFailureFileFetcher struct{}
+
+func (m *MockFailureFileFetcher) GetModifiedFilesFromCommit(ctx context.Context, _ models.Repo, _ string, _ int64) ([]string, error) {
+	return nil, errors.New("some error")
+}
