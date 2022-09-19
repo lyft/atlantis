109feb45bd01dd4943c9f220f7cc9db1432eab4b
Nish Krishnan <nishk@lyft.com>
2022-09-19 13:41:58 -0700

    Add plan summary test.

--- server/neptune/workflows/internal/activities/terraform/summary_test.go
+++ server/neptune/workflows/internal/activities/terraform/summary_test.go
@@ -1,8 +1,40 @@
-package terraform
+package terraform_test
 
-import "testing"
+import (
+	"testing"
 
-//TODO
+	"github.com/runatlantis/atlantis/server/neptune/workflows/internal/activities/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
 func TestSummary(t *testing.T) {
-	
+	plan := "{\"format_version\": \"1.0\",\"resource_changes\":[{\"change\":{\"actions\":[\"update\"]},\"address\":\"type.resource_update\"},{\"change\":{\"actions\":[\"create\"]},\"address\":\"type.resource_create\"}, {\"change\":{\"actions\":[\"delete\"]},\"address\":\"type.resource_delete\"}]}"
+
+	t.Run("success", func(t *testing.T) {
+		summary, err := terraform.NewPlanSummaryFromJSON([]byte(plan))
+		assert.NoError(t, err)
+
+		assert.Equal(t, terraform.PlanSummary{
+			Creations: []terraform.ResourceSummary{
+				{
+					Address: "type.resource_create",
+				},
+			},
+			Updates: []terraform.ResourceSummary{
+				{
+					Address: "type.resource_update",
+				},
+			},
+			Deletions: []terraform.ResourceSummary{
+				{
+					Address: "type.resource_delete",
+				},
+			},
+		}, summary)
+	})
+
+	t.Run("error", func(t *testing.T) {
+		_, err := terraform.NewPlanSummaryFromJSON([]byte("{{"))
+		assert.Error(t, err)
+	})
 }
