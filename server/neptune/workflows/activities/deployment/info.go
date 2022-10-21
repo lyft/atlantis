50e3b86bfc9c91a570a57fb7eae8ad9b8d99bd6d
Nish Krishnan <nishk@lyft.com>
2022-10-20 22:38:48 -0700

    Audit job fixes.

--- server/neptune/workflows/activities/deployment/info.go
+++ server/neptune/workflows/activities/deployment/info.go
@@ -1,19 +1,24 @@
 package deployment
 
-import (
-	"github.com/runatlantis/atlantis/server/neptune/workflows/activities/github"
-	"github.com/runatlantis/atlantis/server/neptune/workflows/activities/terraform"
-)
-
-const InfoSchemaVersion = "1.0.0"
+const InfoSchemaVersion = 1.0
 
 type Info struct {
-	Version        string
-	ID             string
-	CheckRunID     int64
-	Revision       string
-	InitiatingUser github.User
-	Repo           github.Repo
-	Root           terraform.Root
-	Tags           map[string]string
+	Version  int
+	ID       string
+	Revision string
+	Repo     Repo
+	Root     Root
+}
+
+type Repo struct {
+	Owner string
+	Name  string
+}
+
+func (r Repo) GetFullName() string {
+	return r.Owner + "/" + r.Name
+}
+
+type Root struct {
+	Name string
 }
