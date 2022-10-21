908f5f40e7137992b03662b648b5d453d3bc56f3
Nish Krishnan <nishk@lyft.com>
2022-10-21 07:51:29 -0700

    Add comment>

--- server/neptune/workflows/activities/deployment/info.go
+++ server/neptune/workflows/activities/deployment/info.go
@@ -2,6 +2,9 @@ package deployment
 
 const InfoSchemaVersion = 1.0
 
+// These objects are persisted and should be kept as lightweight as possible
+// Additionally, please ensure changes to this object going forward are backwards compatible
+// Version is there to help in case of incompatible changes.
 type Info struct {
 	Version  int
 	ID       string
