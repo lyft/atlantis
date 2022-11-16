01f269a5b258c0a44ffcf042756fb589cb1214db
Samra Belachew <sbelachew@lyft.com>
2022-11-15 17:38:32 -0800

    append-additional-kvs

--- server/neptune/workflows/internal/config/logger/logger.go
+++ server/neptune/workflows/internal/config/logger/logger.go
@@ -9,26 +9,26 @@ func Info(ctx workflow.Context, msg string, additionalKVs ...interface{}) {
 	logger := workflow.GetLogger(ctx)
 	kvs := context.ExtractFieldsAsList(ctx)
 
-	logger.Info(msg, append(kvs, additionalKVs)...)
+	logger.Info(msg, append(kvs, additionalKVs...))
 }
 
 func Warn(ctx workflow.Context, msg string, additionalKVs ...interface{}) {
 	logger := workflow.GetLogger(ctx)
 	kvs := context.ExtractFieldsAsList(ctx)
 
-	logger.Warn(msg, append(kvs, additionalKVs)...)
+	logger.Warn(msg, append(kvs, additionalKVs...))
 }
 
 func Error(ctx workflow.Context, msg string, additionalKVs ...interface{}) {
 	logger := workflow.GetLogger(ctx)
 	kvs := context.ExtractFieldsAsList(ctx)
 
-	logger.Error(msg, append(kvs, additionalKVs)...)
+	logger.Error(msg, append(kvs, additionalKVs...))
 }
 
-func Debug(ctx workflow.Context, msg string) {
+func Debug(ctx workflow.Context, msg string, additionalKVs ...interface{}) {
 	logger := workflow.GetLogger(ctx)
 	kvs := context.ExtractFieldsAsList(ctx)
 
-	logger.Debug(msg, kvs...)
+	logger.Debug(msg, append(kvs, additionalKVs...))
 }
