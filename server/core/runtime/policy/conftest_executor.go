559ba39321c455cb80db99bbba7a8e2821d6d959
Samra Belachew <sbelachew@lyft.com>
2022-12-05 14:19:25 -0800

    conftest-executor

--- server/core/runtime/policy/conftest_executor.go
+++ server/core/runtime/policy/conftest_executor.go
@@ -3,79 +3,83 @@ package policy
 import (
 	"context"
 	"fmt"
-	"github.com/pkg/errors"
-	"github.com/runatlantis/atlantis/server/core/config/valid"
-	"github.com/runatlantis/atlantis/server/events/models"
 	"path/filepath"
 	"strings"
 
-	runtime_models "github.com/runatlantis/atlantis/server/core/runtime/models"
+	"github.com/pkg/errors"
+	"github.com/runatlantis/atlantis/server/core/config/valid"
 	"github.com/runatlantis/atlantis/server/events/command"
+	"github.com/runatlantis/atlantis/server/events/models"
 )
+
+type sourceResolver interface {
+	Resolve(policySet valid.PolicySet) (string, error)
+}
 
 type policyFilter interface {
 	Filter(ctx context.Context, installationToken int64, repo models.Repo, prNum int, failedPolicies []valid.PolicySet) ([]valid.PolicySet, error)
 }
 
+type exec interface {
+	CombinedOutput(args []string, envs map[string]string, workdir string) (string, error)
+}
+
 // ConfTestExecutor runs a versioned conftest binary with the args built from the project context.
 // Project context defines whether conftest runs a local policy set or runs a test on a remote policy set.
 type ConfTestExecutor struct {
-	SourceResolver SourceResolver
-	Exec           runtime_models.Exec
-	policyFilter   policyFilter
+	SourceResolver sourceResolver
+	Exec           exec
+	PolicyFilter   policyFilter
 }
 
+// Run performs conftest policy tests against changes and fails if any policy does not pass. It also runs an all-or-nothing
+// filter that will filter out all policy failures based on the filter criteria.
 func (c *ConfTestExecutor) Run(ctx context.Context, prjCtx command.ProjectContext, executablePath string, envs map[string]string, workdir string, extraArgs []string) (string, error) {
+	var policyArgs []Arg
 	var policyNames []string
 	var failedPolicies []valid.PolicySet
-	var results []string
-	var policyErr error
-
 	inputFile := filepath.Join(workdir, prjCtx.GetShowResultFileName())
 
 	for _, policySet := range prjCtx.PolicySets.PolicySets {
 		path, err := c.SourceResolver.Resolve(policySet)
 		// Let's not fail the whole step because of a single failure. Log and fail silently
 		if err != nil {
-			prjCtx.Log.ErrorContext(prjCtx.RequestCtx, "Error resolving policyset", map[string]interface{}{
-				"policy": policySet.Name,
-				"err":    err.Error(),
-			})
+			prjCtx.Log.ErrorContext(prjCtx.RequestCtx, fmt.Sprintf("Error resolving policyset %s. err: %s", policySet.Name, err.Error()))
 			continue
 		}
-		args := ConftestTestCommandArgs{
-			PolicyArgs: []Arg{NewPolicyArg(path)},
-			ExtraArgs:  extraArgs,
-			InputFile:  inputFile,
-			Command:    executablePath,
-		}
-		serializedArgs, err := args.build()
-		if err != nil {
-			prjCtx.Log.WarnContext(prjCtx.RequestCtx, "No policies have been configured")
-			return "", nil
-			// TODO: enable when we can pass policies in otherwise e2e tests with policy checks fail
-			// return "", errors.Wrap(err, "building args")
-		}
+		policyArgs = append(policyArgs, NewPolicyArg(path))
 		policyNames = append(policyNames, policySet.Name)
-		cmdOutput, err := c.Exec.CombinedOutput(serializedArgs, envs, workdir)
-		if err != nil {
-			failedPolicies = append(failedPolicies, policySet)
-			policyErr = err
-		}
-		results = append(results, cmdOutput)
 	}
+
+	args := ConftestTestCommandArgs{
+		PolicyArgs: policyArgs,
+		ExtraArgs:  extraArgs,
+		InputFile:  inputFile,
+		Command:    executablePath,
+	}
+	serializedArgs, err := args.build()
+	if err != nil {
+		prjCtx.Log.WarnContext(prjCtx.RequestCtx, "No policies have been configured")
+		return "", errors.Wrap(err, "building args")
+	}
+
+	// TODO: run each policy set separately and use each pass/failure decision to populate failedPolicies
+	cmdOutput, policyErr := c.Exec.CombinedOutput(serializedArgs, envs, workdir)
+	if policyErr != nil {
+		failedPolicies = prjCtx.PolicySets.PolicySets
+	}
+
 	title := c.buildTitle(policyNames)
-	results = append([]string{title}, results...)
-	output := c.sanitizeOutput(inputFile, strings.Join(results, "\n"))
+	output := c.sanitizeOutput(inputFile, title+cmdOutput)
 	// TODO: populate installation token into project context
-	failedPolicyNames, err := c.policyFilter.Filter(ctx, 0, prjCtx.HeadRepo, prjCtx.Pull.Num, failedPolicies)
+	failedPolicies, err = c.PolicyFilter.Filter(ctx, 0, prjCtx.HeadRepo, prjCtx.Pull.Num, failedPolicies)
 	if err != nil {
 		prjCtx.Log.ErrorContext(prjCtx.RequestCtx, "error filtering out approved policies", map[string]interface{}{
 			"err": err,
 		})
 		return output, errors.New("internal server error")
 	}
-	if len(failedPolicyNames) == 0 {
+	if len(failedPolicies) == 0 {
 		return output, nil
 	}
 	return output, policyErr
