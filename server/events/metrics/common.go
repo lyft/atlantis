b58a6074d11393b40cb565ae83d2cbddad04cf05
Nish Krishnan <nishk@lyft.com>
2023-02-01 08:08:19 -0800

    Add inbound interceptors for custom activity/signal metrics.

--- server/events/metrics/common.go
+++ server/events/metrics/common.go
@@ -10,6 +10,26 @@ const (
 	FilterAbsentMetric  = "absent"
 	FilterErrorMetric   = "error"
 
-	RootTag = "root"
-	RepoTag = "repo"
+	RootTag         = "root"
+	RepoTag         = "repo"
+	WorkflowTypeTag = "workflow_type"
+	ActivityTypeTag = "activity_type"
+	TaskQueueTag    = "task_queue"
+	NamespaceTag    = "namespace"
+
+	ActivityExecutionSuccess = "activity_execution_success"
+	ActivityExecutionFailure = "activity_execution_failure"
+
+	// Note: This is specifically calculated when the activity starts (not scheduled)
+	ActivityExecutionLatency = "activity_execution_latency"
+
+	SignalNameTag = "signal_name"
+
+	// Signal handling metrics before it is added to a buffered channel
+	SignalHandleSuccess = "signal_handle_success"
+	SignalHandleFailure = "signal_handle_failure"
+	SignalHandleLatency = "signal_handle_latency"
+
+	// Signal receive is when we receive it off the channel
+	SignalReceive = "signal_receive"
 )
