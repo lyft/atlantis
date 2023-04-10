272957ed406cb355a4039285f31bed1b2bbb631b
Nish Krishnan <nishk@lyft.com>
2023-04-10 10:32:30 -0700

    Fix.

--- server/neptune/workflows/internal/deploy/terraform/state.go
+++ server/neptune/workflows/internal/deploy/terraform/state.go
@@ -2,13 +2,11 @@ package terraform
 
 import (
 	"context"
-	"strconv"
 
 	"github.com/pkg/errors"
 	"github.com/runatlantis/atlantis/server/events/metrics"
 
 	"github.com/runatlantis/atlantis/server/neptune/workflows/activities"
-	"github.com/runatlantis/atlantis/server/neptune/workflows/activities/terraform"
 	"github.com/runatlantis/atlantis/server/neptune/workflows/internal/terraform/state"
 	"go.temporal.io/sdk/workflow"
 )
@@ -44,40 +42,4 @@ func (n *StateReceiver) Receive(ctx workflow.Context, c workflow.ReceiveChannel,
 			workflow.GetLogger(ctx).Error(errors.Wrap(err, "notifying workflow state change").Error())
 		}
 	}
-}
-
-func (n *StateReceiver) emitApplyEvents(ctx workflow.Context, jobState *state.Job, deploymentInfo DeploymentInfo) error {
-	var atlantisJobState activities.AtlantisJobState
-	startTime := strconv.FormatInt(jobState.StartTime.Unix(), 10)
-
-	var endTime string
-	switch jobState.Status {
-	case state.InProgressJobStatus:
-		atlantisJobState = activities.AtlantisJobStateRunning
-	case state.SuccessJobStatus:
-		atlantisJobState = activities.AtlantisJobStateSuccess
-		endTime = strconv.FormatInt(jobState.EndTime.Unix(), 10)
-	case state.FailedJobStatus:
-		atlantisJobState = activities.AtlantisJobStateFailure
-		endTime = strconv.FormatInt(jobState.EndTime.Unix(), 10)
-
-	// no need to emit events on other states
-	default:
-		return nil
-	}
-
-	auditJobReq := activities.AuditJobRequest{
-		Repo:           deploymentInfo.Repo,
-		Root:           deploymentInfo.Root,
-		JobID:          jobState.ID,
-		InitiatingUser: deploymentInfo.InitiatingUser,
-		Tags:           deploymentInfo.Tags,
-		Revision:       deploymentInfo.Commit.Revision,
-		State:          atlantisJobState,
-		StartTime:      startTime,
-		EndTime:        endTime,
-		IsForceApply:   deploymentInfo.Root.Trigger == terraform.ManualTrigger && !deploymentInfo.Root.Rerun,
-	}
-
-	return workflow.ExecuteActivity(ctx, n.Activity.AuditJob, auditJobReq).Get(ctx, nil)
 }
