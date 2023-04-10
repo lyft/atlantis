951884d6495952b42df8153fb8d3918dfeb1a9dc
samrabelachew <samrabelachew@users.noreply.github.com>
2023-04-10 09:12:05 -0700

    PR workflow skeleton (#588)
    Creates base workflow definition subsequent PRs will reference as we
    implement the temporal clients on the gateway side. Won't set up the
    worker until we being to actually implement the workflow.
    
    Also, am open to suggestions on a better name than PR workflow. I feel
    like this name may get confusing over time.

---
+++ server/neptune/workflows/pr.go
@@ -0,0 +1,12 @@
+package workflows
+
+import (
+	"github.com/runatlantis/atlantis/server/neptune/workflows/internal/pr"
+	"go.temporal.io/sdk/workflow"
+)
+
+type PRRequest = pr.Request
+
+func PR(ctx workflow.Context, request PRRequest) error {
+	return pr.Workflow(ctx, request)
+}
