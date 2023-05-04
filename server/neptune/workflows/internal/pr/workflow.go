d350c50215697f19e83f0cf4f1b3b2fbbfdc3af4
samrabelachew <samrabelachew@users.noreply.github.com>
2023-05-04 08:48:44 -0700

    Create initial PR worker in temporalworker server (#615)
    Sets up the PR worker + TQ for the new workflows. Similar to the deploy
    worker, we register TF activities to the parent PR worker because we
    wait until the initial TF activity (i.e. `activities.GetWorkerInfo`)
    runs to determine which TQ to use for the remaining TF activities.

--- server/neptune/workflows/internal/pr/workflow.go
+++ server/neptune/workflows/internal/pr/workflow.go
@@ -2,6 +2,8 @@ package pr
 
 import "go.temporal.io/sdk/workflow"
 
+const TaskQueue = "pr"
+
 func Workflow(ctx workflow.Context, request Request) error {
 	// TODO: implement workflow
 	return nil
