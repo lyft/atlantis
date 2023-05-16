f54b1cfeb5fca4c376bd35c13f250abce2e4c106
Samra Belachew <sbelachew@lyft.com>
2023-05-16 09:53:49 -0700

    draft

--- server/neptune/workflows/pr.go
+++ server/neptune/workflows/pr.go
@@ -2,6 +2,7 @@ package workflows
 
 import (
 	"github.com/runatlantis/atlantis/server/neptune/workflows/internal/pr"
+	"github.com/runatlantis/atlantis/server/neptune/workflows/internal/pr/request"
 	"github.com/runatlantis/atlantis/server/neptune/workflows/internal/pr/revision"
 	"go.temporal.io/sdk/workflow"
 )
@@ -9,7 +10,16 @@ import (
 var PRTaskQueue = pr.TaskQueue
 var PRTerraformRevisionSignalID = revision.TerraformRevisionSignalID
 
+const PRDestroyPlanMode = request.DestroyPlanMode
+const PRNormalPlanMode = request.NormalPlanMode
+
 type PRNewRevisionSignalRequest = revision.NewTerraformRevisionRequest
+type PRRepo = request.Repo
+type PRRoot = request.Root
+type PRJob = request.Job
+type PRStep = request.Step
+type PRPlanMode = request.PlanMode
+type PRAppCredentials = request.AppCredentials
 
 type PRRequest = pr.Request
 
