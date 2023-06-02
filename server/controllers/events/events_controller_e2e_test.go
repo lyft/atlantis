a813f06aca7b59e8da39ade8c56827a73cbfa7bc
Samra Belachew <sbelachew@lyft.com>
2023-06-02 09:04:18 -0700

    e2e-flag

--- server/controllers/events/events_controller_e2e_test.go
+++ server/controllers/events/events_controller_e2e_test.go
@@ -406,6 +406,12 @@ func TestGitHubWorkflowWithPolicyCheck(t *testing.T) {
   true: true
   false: false
   default: false
+  trackEvents: false
+legacy-deprecation:
+  percentage: 100
+  true: true
+  false: false
+  default: false
   trackEvents: false`)
 	if testing.Short() {
 		t.SkipNow()
@@ -562,6 +568,12 @@ func TestGitHubWorkflowWithPolicyCheck(t *testing.T) {
 
 func TestGitHubWorkflowPullRequestsWorkflows(t *testing.T) {
 	featureConfig := feature.StringRetriever(`platform-mode:
+  percentage: 100
+  true: true
+  false: false
+  default: false
+  trackEvents: false
+legacy-deprecation:
   percentage: 100
   true: true
   false: false
