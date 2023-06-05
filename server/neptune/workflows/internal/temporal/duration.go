54c93989c199102dbefdaa1d859050528cfa0292
Nish Krishnan <nishk@lyft.com>
2023-06-05 09:50:56 -0700

    lint.

--- server/neptune/workflows/internal/temporal/duration.go
+++ server/neptune/workflows/internal/temporal/duration.go
@@ -11,7 +11,8 @@ type NextDay func(time.Time) time.Time
 func NextBusinessDay(d time.Time) time.Time {
 	d = d.Add(24 * time.Hour)
 
-	for ; d.Weekday() == time.Saturday || d.Weekday() == time.Sunday; d = d.Add(24 * time.Hour) {}
+	for ; d.Weekday() == time.Saturday || d.Weekday() == time.Sunday; d = d.Add(24 * time.Hour) {
+	}
 	return d
 }
 
--- server/neptune/workflows/internal/temporal/duration_test.go
+++ server/neptune/workflows/internal/temporal/duration_test.go
@@ -16,35 +16,35 @@ func TestNextBusinessDay(t *testing.T) {
 	}{
 		// thursday
 		{
-			date: time.Date(2023, time.June, 1, 10, 0, 0, 0, time.Local),
+			date:            time.Date(2023, time.June, 1, 10, 0, 0, 0, time.Local),
 			nextBusinessDay: time.Date(2023, time.June, 2, 10, 0, 0, 0, time.Local),
 		},
 
 		// friday
 		{
-			date: time.Date(2023, time.June, 2, 10, 0, 0, 0, time.Local),
+			date:            time.Date(2023, time.June, 2, 10, 0, 0, 0, time.Local),
 			nextBusinessDay: time.Date(2023, time.June, 5, 10, 0, 0, 0, time.Local),
 		},
 
 		// saturday
 		{
-			date: time.Date(2023, time.June, 3, 10, 0, 0, 0, time.Local),
+			date:            time.Date(2023, time.June, 3, 10, 0, 0, 0, time.Local),
 			nextBusinessDay: time.Date(2023, time.June, 5, 10, 0, 0, 0, time.Local),
 		},
 		// sunday
 		{
-			date: time.Date(2023, time.June, 4, 10, 0, 0, 0, time.Local),
+			date:            time.Date(2023, time.June, 4, 10, 0, 0, 0, time.Local),
 			nextBusinessDay: time.Date(2023, time.June, 5, 10, 0, 0, 0, time.Local),
 		},
 	}
 
 	for _, c := range cases {
 		ca := c
-		
+
 		t.Run("", func(t *testing.T) {
 			result := temporal.NextBusinessDay(ca.date)
 			assert.Equal(t, ca.nextBusinessDay, result)
 		})
 	}
-	
+
 }
