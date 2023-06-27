25552ca20fafa34993038ef22afd26317d0f6eba
Samra Belachew <sbelachew@lyft.com>
2023-06-27 14:41:16 -0700

    lint

--- server/neptune/workflows/activities/conftest/summary.go
+++ server/neptune/workflows/activities/conftest/summary.go
@@ -52,5 +52,5 @@ func (s ValidateSummary) String() string {
 	}
 
 	return fmt.Sprintf(
-		"Successful policies: %s`\n\n`"+"Failing policies: %s", successes, failures)
+		"Successful policies: %s`\n\n`Failing policies: %s", successes, failures)
 }
