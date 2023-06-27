aa375302fb91bdd476bcecac72d0ef88de42e34f
Samra Belachew <sbelachew@lyft.com>
2023-06-27 15:12:35 -0700

    linter

--- server/neptune/gateway/event/legacy_comment_handler.go
+++ server/neptune/gateway/event/legacy_comment_handler.go
@@ -8,7 +8,6 @@ import (
 	"github.com/runatlantis/atlantis/server/legacy/events/command"
 	"github.com/runatlantis/atlantis/server/legacy/http"
 	"github.com/runatlantis/atlantis/server/logging"
-	"github.com/runatlantis/atlantis/server/models"
 )
 
 type LegacyCommentHandler struct {
@@ -27,24 +26,6 @@ func (p *LegacyCommentHandler) Handle(ctx context.Context, event Comment, cmd *c
 		return errors.Wrap(err, "forwarding request through sns")
 	}
 	return nil
-}
-
-func (p *LegacyCommentHandler) shouldMarkEventQueued(event Comment, cmd *command.Comment) bool {
-	// pending status should only be for plan step
-	if cmd.Name != command.Plan {
-		return false
-	}
-	// pull event should not be from a fork
-	if event.Pull.HeadRepo.Owner != event.Pull.BaseRepo.Owner {
-		return false
-	}
-	// pull event should not be from closed PR
-	if event.Pull.State == models.ClosedPullState {
-		return false
-	}
-	// pull event should not use an invalid base branch
-	repo := p.globalCfg.MatchingRepo(event.Pull.BaseRepo.ID())
-	return repo.BranchMatches(event.Pull.BaseBranch)
 }
 
 func (p *LegacyCommentHandler) ForwardToSns(ctx context.Context, request *http.BufferedRequest) error {
