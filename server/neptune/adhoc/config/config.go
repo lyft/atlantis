9f0733d13d43694ab945f559bc1b9c1559506805
Shawna Monero <smonero@lyft.com>
2024-03-07 12:39:18 -0800

    still making progress

---
+++ server/neptune/adhoc/config/config.go
@@ -0,0 +1,27 @@
+package adhocconfig
+
+import (
+	"github.com/palantir/go-githubapp/githubapp"
+	"github.com/runatlantis/atlantis/server/config/valid"
+	"github.com/runatlantis/atlantis/server/logging"
+	neptune "github.com/runatlantis/atlantis/server/neptune/temporalworker/config"
+)
+
+// Config is TerraformAdmin (Adhoc mode) specific user config
+type Config struct {
+	AuthCfg          neptune.AuthConfig
+	ServerCfg        neptune.ServerConfig
+	FeatureConfig    neptune.FeatureConfig
+	TemporalCfg      valid.Temporal
+	GithubCfg        valid.Github
+	TerraformCfg     neptune.TerraformConfig
+	DeploymentConfig valid.StoreConfig
+	JobConfig        valid.StoreConfig
+	Metrics          valid.Metrics
+
+	StatsNamespace string
+
+	DataDir   string
+	CtxLogger logging.Logger
+	App       githubapp.Config
+}
