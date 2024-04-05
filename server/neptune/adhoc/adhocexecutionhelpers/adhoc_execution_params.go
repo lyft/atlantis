3a231b90e640bc9e680e196d74426b1028da4528
Shawna Monero <66325812+smonero@users.noreply.github.com>
2024-04-05 09:27:40 -0700

    [DEVCON-6918] Helper funcs for getting the github repo (#743)

--- server/neptune/adhoc/adhocexecutionhelpers/adhoc_execution_params.go
+++ server/neptune/adhoc/adhocexecutionhelpers/adhoc_execution_params.go
@@ -1,15 +1,30 @@
 package adhoc
 
 import (
+	"context"
+
+	"github.com/pkg/errors"
+	"github.com/runatlantis/atlantis/server/neptune/adhoc/adhocgithubhelpers"
 	"github.com/runatlantis/atlantis/server/neptune/workflows/activities/github"
 	"github.com/runatlantis/atlantis/server/neptune/workflows/activities/terraform"
 )
 
 type AdhocTerraformWorkflowExecutionParams struct {
-	AtlantisRoot  string
-	AtlantisRepo  string
-	Revision      string
-	TerraformRoot terraform.Root
-	GithubRepo    github.Repo
+	Revision       string
+	TerraformRoots []terraform.Root
+	GithubRepo     github.Repo
 	// Note that deploymentID is used in NewWorkflowStore(), but we don't care about that in adhoc mode so can leave it blank
+}
+
+func ConstructAdhocExecParamsWithRootCfgBuilderAndRepoRetriever(ctx context.Context, repoName string, revision string, githubRetriever *adhocgithubhelpers.AdhocGithubRetriever) (AdhocTerraformWorkflowExecutionParams, error) {
+	// TODO: in the future, could potentially pass in the owner instead of hardcoding lyft
+	repo, err := githubRetriever.GetRepository(ctx, "lyft", repoName)
+	if err != nil {
+		return AdhocTerraformWorkflowExecutionParams{}, errors.Wrap(err, "getting repo")
+	}
+
+	return AdhocTerraformWorkflowExecutionParams{
+		Revision:   revision,
+		GithubRepo: repo,
+	}, nil
 }
