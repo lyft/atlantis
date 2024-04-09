798e156364f3d0700b553f570eb548fc92bcdc77
Shawna Monero <smonero@lyft.com>
2024-04-09 01:22:52 -0700

    ok

--- server/neptune/adhoc/adhocexecutionhelpers/adhoc_execution_params.go
+++ server/neptune/adhoc/adhocexecutionhelpers/adhoc_execution_params.go
@@ -5,8 +5,11 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/runatlantis/atlantis/server/neptune/adhoc/adhocgithubhelpers"
+	"github.com/runatlantis/atlantis/server/neptune/gateway/config"
+	root_config "github.com/runatlantis/atlantis/server/neptune/gateway/config"
 	"github.com/runatlantis/atlantis/server/neptune/workflows/activities/github"
 	"github.com/runatlantis/atlantis/server/neptune/workflows/activities/terraform"
+	internal_gh "github.com/runatlantis/atlantis/server/vcs/provider/github"
 )
 
 type AdhocTerraformWorkflowExecutionParams struct {
@@ -16,15 +19,32 @@ type AdhocTerraformWorkflowExecutionParams struct {
 	// Note that deploymentID is used in NewWorkflowStore(), but we don't care about that in adhoc mode so can leave it blank
 }
 
-func ConstructAdhocExecParamsWithRootCfgBuilderAndRepoRetriever(ctx context.Context, repoName string, revision string, githubRetriever *adhocgithubhelpers.AdhocGithubRetriever) (AdhocTerraformWorkflowExecutionParams, error) {
+func ConstructAdhocExecParamsWithRootCfgBuilderAndRepoRetriever(ctx context.Context, repoName string, revision string, githubRetriever *adhocgithubhelpers.AdhocGithubRetriever, rootCfgBuilder *root_config.Builder) (AdhocTerraformWorkflowExecutionParams, error) {
 	// TODO: in the future, could potentially pass in the owner instead of hardcoding lyft
 	repo, err := githubRetriever.GetRepository(ctx, "lyft", repoName)
 	if err != nil {
 		return AdhocTerraformWorkflowExecutionParams{}, errors.Wrap(err, "getting repo")
 	}
 
+	opts := config.BuilderOptions{
+		RepoFetcherOptions: &internal_gh.RepoFetcherOptions{
+			CloneDepth: 1,
+		},
+	}
+
+	rootCfgs, err := rootCfgBuilder.Build(ctx, &root_config.RepoCommit{}, repo.Credentials.InstallationToken, opts)
+	if err != nil {
+		return AdhocTerraformWorkflowExecutionParams{}, errors.Wrap(err, "building root cfgs")
+	}
+
+	roots, err := getRootsFromMergedProjectCfgs(rootCfgs)
+	if err != nil {
+		return AdhocTerraformWorkflowExecutionParams{}, errors.Wrap(err, "getting roots from merged project cfgs")
+	}
+
 	return AdhocTerraformWorkflowExecutionParams{
-		Revision:   revision,
-		GithubRepo: repo,
+		Revision:       revision,
+		GithubRepo:     repo,
+		TerraformRoots: roots,
 	}, nil
 }
--- server/neptune/adhoc/adhocexecutionhelpers/root_helpers.go
+++ server/neptune/adhoc/adhocexecutionhelpers/root_helpers.go
@@ -2,18 +2,11 @@ package adhoc
 
 import (
 	"github.com/runatlantis/atlantis/server/config/valid"
-	"github.com/runatlantis/atlantis/server/neptune/adhoc/request"
 	"github.com/runatlantis/atlantis/server/neptune/gateway/deploy"
+	"github.com/runatlantis/atlantis/server/neptune/workflows"
 	"github.com/runatlantis/atlantis/server/neptune/workflows/activities/execute"
 	"github.com/runatlantis/atlantis/server/neptune/workflows/activities/terraform"
 )
-
-type Repo = request.Repo
-type Root = request.Root
-type Job = request.Job
-type Step = request.Step
-type AppCredentials = request.AppCredentials
-type PlanMode = request.PlanMode
 
 func getRootsFromMergedProjectCfgs(rootCfgs []*valid.MergedProjectCfg) ([]terraform.Root, error) {
 	roots := make([]terraform.Root, 0, len(rootCfgs))
@@ -26,29 +19,28 @@ func getRootsFromMergedProjectCfgs(rootCfgs []*valid.MergedProjectCfg) ([]terraf
 	return roots, nil
 }
 
-func convertMergedProjectCfgToRoot(rootCfg *valid.MergedProjectCfg) request.Root {
+func convertMergedProjectCfgToRoot(rootCfg *valid.MergedProjectCfg) workflows.Root {
 	var tfVersion string
 	if rootCfg.TerraformVersion != nil {
 		tfVersion = rootCfg.TerraformVersion.String()
 	}
 
-	return request.Root{
+	return workflows.Root{
 		Name: rootCfg.Name,
-		Plan: request.Job{
+		Plan: workflows.Job{
 			Steps: prependPlanEnvSteps(rootCfg),
 		},
-		Apply: request.Job{
+		Apply: workflows.Job{
 			Steps: generateSteps(rootCfg.DeploymentWorkflow.Apply.Steps),
 		},
 		RepoRelPath:  rootCfg.RepoRelDir,
 		TrackedFiles: rootCfg.WhenModified,
 		TfVersion:    tfVersion,
-		PlanMode:     generatePlanMode(rootCfg),
-		// note we don't set TriggerInfo
+		// note we don't set TriggerInfo or PlanMode
 	}
 }
 
-func convertToTerraformRoot(root Root) terraform.Root {
+func convertToTerraformRoot(root workflows.AdhocRoot) terraform.Root {
 	return terraform.Root{
 		Name: root.Name,
 		Apply: execute.Job{
@@ -58,18 +50,18 @@ func convertToTerraformRoot(root Root) terraform.Root {
 			Job: execute.Job{
 				Steps: steps(root.Plan.Steps)},
 		},
-		// Note we don't have mode nor PlanApproval
+		// Note we don't have mode, nor PlanApproval
 		Path:      root.RepoRelPath,
 		TfVersion: root.TfVersion,
 	}
 }
 
 // These are copied here so that we don't have to use a workflowsignaler
-func prependPlanEnvSteps(cfg *valid.MergedProjectCfg) []Step {
-	var steps []Step
+func prependPlanEnvSteps(cfg *valid.MergedProjectCfg) []workflows.Step {
+	var steps []workflows.Step
 	if t, ok := cfg.Tags[deploy.Manifest]; ok {
 		//this is a Lyft specific env var
-		steps = append(steps, Step{
+		steps = append(steps, workflows.Step{
 			StepName:    deploy.EnvStep,
 			EnvVarName:  "MANIFEST_FILEPATH",
 			EnvVarValue: t,
@@ -79,10 +71,10 @@ func prependPlanEnvSteps(cfg *valid.MergedProjectCfg) []Step {
 	return steps
 }
 
-func generateSteps(steps []valid.Step) []Step {
-	var workflowSteps []Step
+func generateSteps(steps []valid.Step) []workflows.Step {
+	var workflowSteps []workflows.Step
 	for _, step := range steps {
-		workflowSteps = append(workflowSteps, Step{
+		workflowSteps = append(workflowSteps, workflows.Step{
 			StepName:    step.StepName,
 			ExtraArgs:   step.ExtraArgs,
 			RunCommand:  step.RunCommand,
@@ -93,16 +85,7 @@ func generateSteps(steps []valid.Step) []Step {
 	return workflowSteps
 }
 
-func generatePlanMode(cfg *valid.MergedProjectCfg) request.PlanMode {
-	t, ok := cfg.Tags[deploy.Deprecated]
-	if ok && t == deploy.Destroy {
-		return request.DestroyPlanMode
-	}
-
-	return request.NormalPlanMode
-}
-
-func steps(requestSteps []request.Step) []execute.Step {
+func steps(requestSteps []workflows.Step) []execute.Step {
 	var terraformSteps []execute.Step
 	for _, step := range requestSteps {
 		terraformSteps = append(terraformSteps, execute.Step{
